e9bced144a8202059bf9bec036363e46ceb674a5
maiara filip <filipmaiara@gmail.com>
2022-04-04 10:37:17 -0300

    1.0.0

---
+++ calculadora.js
@@ -0,0 +1,21 @@
+var somar = require("./modulos/somar");
+
+console.log(somar(20,90));
+
+//
+
+var subtrair = require("./modulos/subtrair");
+
+console.log(subtrair(20,90));
+
+//
+
+var multiplicar = require("./modulos/multiplicar");
+
+console.log(multiplicar(20,90));
+
+//
+
+var dividir = require("./modulos/dividir");
+
+console.log(dividir(20,90));
